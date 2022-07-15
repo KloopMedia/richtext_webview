94bf8538a0bc505a157fed2480c4da2e9aaac638
atna <atay.narynov@gmail.com>
2022-07-15 20:55:25 +0600

    Set inital text to ''

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Buffer } from 'buffer';
 
 function App() {
-  const [text, setText] = useState('<p>atai</p>');
+  const [text, setText] = useState('');
 
 
   useEffect(() => {
